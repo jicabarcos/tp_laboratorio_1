51319e1f1d8d1f02be6d11aef4ec29a142c84807
jicabarcos <66450407+jicabarcos@users.noreply.github.com>
2020-11-08 23:50:17 -0300

    Commit FINAL (falto doxy)

--- TP 3/Controller.c
+++ TP 3/Controller.c
@@ -5,14 +5,6 @@
 #include "Employee.h"
 #include "parser.h"
 
-
-/** \brief Carga los datos de los empleados desde el archivo data.csv (modo texto).
- *
- * \param path char*
- * \param pArrayListEmployee LinkedList*
- * \return int
- *
- */
 int controller_loadFromText(char* path , LinkedList* pArrayListEmployee)
 {
 
@@ -34,13 +26,6 @@ int controller_loadFromText(char* path , LinkedList* pArrayListEmployee)
 
 }
 
-/** \brief Carga los datos de los empleados desde el archivo data.csv (modo binario).
- *
- * \param path char*
- * \param pArrayListEmployee LinkedList*
- * \return int
- *
- */
 int controller_loadFromBinary(char* path , LinkedList* pArrayListEmployee)
 {
 
@@ -62,13 +47,6 @@ int controller_loadFromBinary(char* path , LinkedList* pArrayListEmployee)
 
 }
 
-/** \brief Alta de empleados
- *
- * \param path char*
- * \param pArrayListEmployee LinkedList*
- * \return int
- *
- */
 int controller_addEmployee(LinkedList* pArrayListEmployee)
 {
 
@@ -88,25 +66,6 @@ int controller_addEmployee(LinkedList* pArrayListEmployee)
 
 }
 
-/** \brief Modificar datos de empleado
- *
- * \param path char*
- * \param pArrayListEmployee LinkedList*
- * \return int
- *
- */
-int controller_editEmployee(LinkedList* pArrayListEmployee)
-{
-    return 1;
-}
-
-/** \brief Baja de empleado
- *
- * \param path char*
- * \param pArrayListEmployee LinkedList*
- * \return int
- *
- */
 int controller_removeEmployee(LinkedList* pArrayListEmployee)
 {
 
@@ -123,13 +82,6 @@ int controller_removeEmployee(LinkedList* pArrayListEmployee)
 
 }
 
-/** \brief Listar empleados
- *
- * \param path char*
- * \param pArrayListEmployee LinkedList*
- * \return int
- *
- */
 int controller_ListEmployee(LinkedList* pArrayListEmployee)
 {
 
@@ -145,13 +97,6 @@ int controller_ListEmployee(LinkedList* pArrayListEmployee)
 
 }
 
-/** \brief Ordenar empleados
- *
- * \param path char*
- * \param pArrayListEmployee LinkedList*
- * \return int
- *
- */
 int controller_sortEmployee(LinkedList* pArrayListEmployee)
 {
 
@@ -220,13 +165,6 @@ int controller_sortEmployee(LinkedList* pArrayListEmployee)
 
 }
 
-/** \brief Guarda los datos de los empleados en el archivo data.csv (modo texto).
- *
- * \param path char*
- * \param pArrayListEmployee LinkedList*
- * \return int
- *
- */
 int controller_saveAsText(char* path , LinkedList* pArrayListEmployee)
 {
 
@@ -248,13 +186,6 @@ int controller_saveAsText(char* path , LinkedList* pArrayListEmployee)
 
 }
 
-/** \brief Guarda los datos de los empleados en el archivo data.csv (modo binario).
- *
- * \param path char*
- * \param pArrayListEmployee LinkedList*
- * \return int
- *
- */
 int controller_saveAsBinary(char* path , LinkedList* pArrayListEmployee)
 {
 
